13ec0877fc4c508099f18822829e44495a0f6057
Denys Nesterenko <106149933+JeyRichmond@users.noreply.github.com>
2023-04-27 14:15:08 +0200

    Create SalesPerson.cs

---
+++ SalesPerson.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InheritanceTask
+{
+    public class SalesPerson : Employee
+    {
+        private readonly int percent;
+
+        public SalesPerson(string name, decimal salary, int percent) : base (name, salary)
+        {
+            this.percent = percent;
+        }
+
+        public override void SetBonus(decimal bonus)
+        {
+            if (percent > 100 && percent < 200)
+            {
+                bonus *= 2;
+            }
+            else if (percent > 200)
+            {
+                bonus *= 3;
+            }
+            base.SetBonus(bonus);
+        }
+    }
+}
